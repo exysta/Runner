5343c05c80aef7c5e3a50e7af56c2ec474e746e2
exysta <lucas.chapart@ensea.fr>
2023-11-10 15:14:55 +0100

    jolie background

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
         @Override
 public void start(Stage primaryStage) throws Exception{
-
         primaryStage.setTitle("Hello world");
-        Group root = new Group();
-        Pane pane = new Pane(root);
-        Scene theScene = new Scene(pane, 1000, 600,true);
-        primaryStage.setScene(theScene);
+        Image spriteSheet = new Image("space.png");
+        Image spriteSheet2 = new Image("space2.png");
+        ImageView sprite1 = new ImageView(spriteSheet);
+        ImageView sprite2 = new ImageView(spriteSheet2);
+        ArrayList <ImageView> sprites= new ArrayList<ImageView>();
+        sprite1.setImage(spriteSheet);
+        sprite1.setX(-800);
+        sprite1.setY(0);
 
+        sprite2.setImage(spriteSheet2);
+        sprite2.setX(800);
+        sprite2.setY(0);
+
+        Group root = new Group(sprite1,sprite2);
+        Pane pane = new Pane(root);
+        Scene theScene = new Scene(pane, 1600, 600,true);
+        primaryStage.setScene(theScene);
         primaryStage.show();
         }
          public static void main(String[] args) {
