85e9f6619e43647281ea5bf24ad21dda820c26a6
exysta <lucas.chapart@ensea.fr>
2023-11-10 15:18:28 +0100

    jolie background

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-
 public class Main extends Application {
 
         @Override
