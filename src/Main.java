f2e3757f2dfba0bd6bce49a3a7746ee4f7e79b99
exysta <exysta@gmail.com>
2023-11-13 23:56:26 +0100

    update 13/11/2023

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,36 @@ import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+//https://zetcode.com/gui/javafx/animation/
         @Override
 public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hello world");
         Image spriteSheet = new Image("space.png");
+        Image herosheet = new Image("hero.png");
         Image spriteSheet2 = new Image("space2.png");
         ImageView sprite1 = new ImageView(spriteSheet);
         ImageView sprite2 = new ImageView(spriteSheet2);
-
+        ImageView spritehero = new ImageView(herosheet);
         sprite1.setX(-800);
         sprite1.setY(0);
 
         sprite2.setX(800);
         sprite2.setY(0);
 
+        Path path = new Path();
         Group root = new Group(sprite1,sprite2);
-        Pane pane = new Pane(root);
+        Group Hero = new Group(spritehero)
+        Pane pane = new Pane(root,Hero);
         GameScene theScene = new GameScene(pane, 1600, 600,true,0,0,40,40,1400,0);
 
         root.getChildren().add(GameScene.getHeart1().getImage());
         root.getChildren().add(GameScene.getHeart2().getImage());
         root.getChildren().add(GameScene.getHeart3().getImage());
+        root.getChildren().add(GameScene.getBackground_r().getImage());
         root.getChildren().add(GameScene.getBackground_r().getImage());
 
         primaryStage.setScene(theScene);
